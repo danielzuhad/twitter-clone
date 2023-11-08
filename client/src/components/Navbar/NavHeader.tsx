66462d2e487a366d1440abee121f22d1299d683d
danielzuhad <danielzuhad@gmail.com>
2023-11-08 20:09:54 +0700

    fixing nav side header

--- client/src/components/Navbar/NavHeader.tsx
+++ client/src/components/Navbar/NavHeader.tsx
@@ -31,7 +31,7 @@ export const NavHeader = ({ navbarItem, className }: Props) => {
           "w-[95vw] h-[7em] p-3 fixed md:left-[1.7%]  sm:left-[2.5%] left-[2.5%]  top-0 bg-white rounded-b-md  border-black  border-b-2  border-l-2  border-r-2 "
         )}
       >
-        <button onClick={handleOpenNavbar}>
+        <button onClick={handleOpenNavbar} className="">
           <img
             src={foto}
             className="w-[5em] h-[5em] object-cover rounded-full"
@@ -47,34 +47,34 @@ export const NavHeader = ({ navbarItem, className }: Props) => {
           className={cn(
             "fixed w-screen h-[110vh] bg-[#060606a8] top-0 left-0 right-0 "
           )}
-        >
-          <div
-            className={cn(
-              "h-screen w-[40vw] p-3 bg-white border-r-2 border-black fixed transition-all -translate-x-full",
-              navbarActive && "translate-x-0 "
-            )}
-          >
-            <button onClick={handleClickNav} className="text-[2.5em] ">
-              <RxCross1 />
-            </button>
-
-            <div className=" flex flex-col gap-5 mt-10">
-              {navbarItem?.map((item: NavbarItem, index) => (
-                <button onClick={handleClickNav}>
-                  <NavSideItem
-                    link={item.link}
-                    title={item.title}
-                    Homeicon={item.Homeicon}
-                    ProfileIcon={item.ProfileIcon}
-                    key={index}
-                    className="border-2 shadow-none"
-                  />
-                </button>
-              ))}
-            </div>
-          </div>
-        </div>
+        />
       )}
+
+      <div
+        className={cn(
+          "h-screen w-[40vw] p-3 bg-white border-r-2 border-black fixed transition-all -translate-x-full left-0 top-0",
+          navbarActive && "translate-x-0 "
+        )}
+      >
+        <button onClick={handleClickNav} className="text-[2.5em] ">
+          <RxCross1 />
+        </button>
+
+        <div className=" flex flex-col gap-5 mt-10">
+          {navbarItem?.map((item: NavbarItem, index) => (
+            <button onClick={handleClickNav}>
+              <NavSideItem
+                link={item.link}
+                title={item.title}
+                Homeicon={item.Homeicon}
+                ProfileIcon={item.ProfileIcon}
+                key={index}
+                className="border-2 shadow-none"
+              />
+            </button>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
